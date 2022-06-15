2c15798d655b7f68c4d13530457717e4404600e9
stellaw99 <101327807+stellaw99@users.noreply.github.com>
2022-06-15 07:26:19 +0200

    added other tabs that could be used in a to-do list

--- main.js
+++ main.js
@@ -19,12 +19,32 @@ function formatTime(date) {
 
 function formatDate(date) {
     const DAYS = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"];
     
     const MONTHS = [
-    "January", "February", "March", "April", "May", "June", "July","August","September", "October", "November", "December"];
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"];
     
-    return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+    return `${DAYS[date.getDay()]}, 
+    ${MONTHS[date.getMonth()]} 
+    ${date.getDate()} 
+    ${date.getFullYear()}`;
 }
 setInterval(() => {
     const now = new Date();
